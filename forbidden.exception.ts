073a2d88c81bdf3254c46133d793b796cd15def2
lemon571 <91371812+lemon571@users.noreply.github.com>
2023-05-28 00:12:34 +0300

    Update forbidden.exception.ts

--- forbidden.exception.ts
+++ forbidden.exception.ts
@@ -1,7 +1,1 @@
-import { HttpException, HttpStatus } from "@nestjs/common";
 
-export class ForbiddenException extends HttpException {
-    constructor() {
-        super('You are forbidden to access the endpoint', HttpStatus.FORBIDDEN);
-    }
-}
