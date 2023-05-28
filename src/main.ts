c6d09ca37e0fc911c0f3b3d64af7246b843ba0f7
lemon571 <91371812+lemon571@users.noreply.github.com>
2023-05-28 11:58:04 +0300

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,12 @@ import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { HttpExceptionFilter } from './exceptions/http-exeption.filter';
+
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalFilters(new HttpExceptionFilter());
   const config = new DocumentBuilder()
     .setTitle('Education API')
     .setVersion('1.0')
